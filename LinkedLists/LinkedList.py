49b5bf22a21115aa30eb4d5b91a540a4359b948f
Angela Zhu <xiaolinangela@gmail.com>
2020-04-27 14:49:58 -0700

    updated LL

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -2,8 +2,10 @@ class LinkedListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
     def getNextNode(self):
-       return self.next
+        return self.next
+
 
 class LinkedList:
     def __init__(self, values=None):
@@ -11,21 +13,27 @@ class LinkedList:
         self.tail = None
         if values != None:
             self.add_multiple(values)
+
     def printNode(self):
-       curr = self.head
-       while curr:
-           print(curr.val)
-           curr = curr.getNextNode()
+        curr = self.head
+        list = []
+        while curr:
+            list.append(curr.val)
+            curr = curr.getNextNode()
+        print(list)
+
     def add_multiple(self, values):
         for v in values:
             self.add(v)
-    def add(self,value):
+
+    def add(self, value):
         if self.head == None:
             self.head = self.tail = LinkedListNode(value)
         else:
             self.tail.next = LinkedListNode(value)
             self.tail = self.tail.next
         return self.tail
+
 
 if __name__ == "__main__":
     l1 = LinkedList([3])
