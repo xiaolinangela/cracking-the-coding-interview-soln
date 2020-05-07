1475a81ef1b9d004824f8965f3ed9b1ecc6cd98b
Angela Zhu <xiaolinangela@gmail.com>
2020-05-07 11:58:41 -0700

    BFS

--- Ch4-TreesAndGraphs/4.1-RouteBetweenNodes.py
+++ Ch4-TreesAndGraphs/4.1-RouteBetweenNodes.py
@@ -8,27 +8,30 @@ class Graph:
     def addEdge(self,u,v):
         self.graph[u].append(v)
     
-    def BFSUntil(self,t,visited):
-        while visited.qsize() != 0: 
-            u = visited.get()
-            for i in self.graph[u]:
-                if i == t:
-                    return True
-                visited.put(i)
-        return False
-    
     def BFS(self,s,t):
-        visited = queue.Queue()
-        visited.put(s)
-        print(self.BFSUntil(t,visited))
-    
+        q = queue.Queue()
+        q.put(s)
+        visited = set()
+        visited.add(s)
+        while q.qsize() != 0: 
+            u = q.get()
+            for i in self.graph[u]:
+                if i not in visited:
+                    if i == t:
+                        return True
+                    else:
+                        q.put(i)
+                # print(list(q.queue))
+                visited.add(i)
+        return False
         
 
 if __name__ == "__main__":
     g = Graph()
     g.addEdge(1,2)
-    g.addEdge(1,3)
+    g.addEdge(2,3)
     g.addEdge(2,4)
-    g.addEdge(2,5)
+    g.addEdge(3,1)
     g.addEdge(3,6)
-    g.BFS(1,16)
+    g.addEdge(6,5)
+    print(g.BFS(1,5))
