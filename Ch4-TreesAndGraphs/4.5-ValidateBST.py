188b444e0098ca80938f4c83ec4aabaff5a2ffd8
Angela Zhu <xiaolinangela@gmail.com>
2020-05-09 22:41:28 -0700

    added recursion method

--- Ch4-TreesAndGraphs/4.5-ValidateBST.py
+++ Ch4-TreesAndGraphs/4.5-ValidateBST.py
@@ -13,18 +13,28 @@ def ValidateBST(root):
         if not root:
             continue
         val = root.val 
-        if val < lower or val >= upper:
+        if val <= lower or val > upper:
             return False
-        
         stack.append((root.right, val, upper))
         stack.append((root.left, lower, val))
-    
     return True
+
+def ValidateBST_recursion(root, lower, upper):
+    if not root:
+        return True
+    if root.val <= lower or root.val > upper:
+        return False
+    return ValidateBST_recursion(root.left, lower, root.val) and ValidateBST_recursion(root.right, root.val, upper)
+
 
 if __name__ == "__main__":
     t = TreeNode(3)
     t.right = TreeNode(10)
-    t.right.left = TreeNode(5)
+    t.right.left = TreeNode(1)
     t.left = TreeNode(2)
     print(ValidateBST(t))
+    print(ValidateBST_recursion(t, -1000, 1000))
     
+
+# TimeComplexity O(n)
+# SpaceComplexity O(n)
