41db2708a52ee958c42d488ada13f56eb7b78469
Angela Zhu <xiaolinangela@gmail.com>
2020-05-11 15:29:52 -0700

    Inorder successor BST

--- Ch4-TreesAndGraphs/4.6-InorderSuccessorBST.py
+++ Ch4-TreesAndGraphs/4.6-InorderSuccessorBST.py
@@ -5,4 +5,25 @@ class TreeNode:
         self.right = right
 
 
-def successor(root):
+def successor(root,p):
+    successor = None
+    while root:
+        if p.val < root.val:
+            successor = root
+            root = root.left
+        else:
+            root = root.right
+        
+    return successor
+
+if __name__ == "__main__":
+    t = TreeNode(5)
+    t.left = TreeNode(3)
+    t.left.right = TreeNode(4)
+    t.right = TreeNode(6)
+    p = t.left.right
+    result = successor(t,p)
+    if result:
+        print(result.val)
+    else:
+        print("None")
