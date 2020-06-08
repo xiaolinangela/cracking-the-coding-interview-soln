72d756ae73863f5ba920e626a0d874f45e5bdea5
Angela Zhu <xiaolinangela@gmail.com>
2020-06-08 15:23:52 -0700

    PermWDups

--- Ch8_RecursionDP/8.8-PermWithDups.py
+++ Ch8_RecursionDP/8.8-PermWithDups.py
@@ -17,11 +17,12 @@ def perm(nums):
                 a = copy.copy(i)
                 a.insert(j, nums[-1])
                 new_result.append(a)
-                if nums[-1] == i[j-1]:
-                    j += 2
+                if j != len(nums)-1 and nums[-1] == i[j]:
+                    j = len(nums)
                 else:
                     j += 1
-        # print(new_result)
+
+                    # print(new_result)
         return new_result
 
 
@@ -29,4 +30,18 @@ if __name__ == "__main__":
     nums = [2, 2, 1, 1]
     print(perm(nums))
 
-122 212 221    1122 1212 1221
+# 122 212 221    1122 1212 1221    1212 2112 2121    1221 2121 2211
+# 122 212 221
+
+
+# len(nums) = 4
+# j = 0
+# i = 122
+# nums[-1] = 1
+
+# j = 1
+# i[0] = 1
+
+# 1122
+# 1212 2112
+# 1221 2121 2211
