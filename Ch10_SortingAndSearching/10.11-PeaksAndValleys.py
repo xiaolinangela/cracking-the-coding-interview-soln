bf27f0fc6c65f0b524466078c3631ea821e8f7e4
Angela Zhu <xiaolinangela@gmail.com>
2020-06-17 17:35:30 -0700

    Peaks&Valleys

---
+++ Ch10_SortingAndSearching/10.11-PeaksAndValleys.py
@@ -0,0 +1,18 @@
+def pv(arr):
+    peak = []
+    valley = []
+    i = 0
+    while i < len(arr) - 1:
+        while i < len(arr) - 1 and arr[i] >= arr[i+1]:
+            i += 1
+        valley.append(arr[i])
+        while i < len(arr) - 1 and arr[i] <= arr[i+1]:
+            i += 1
+        peak.append(arr[i])
+
+    return peak, valley
+
+
+if __name__ == "__main__":
+    arr = [5, 8, 6, 2, 3, 4, 6]
+    print(pv(arr))
